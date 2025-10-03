4c0d1644228bda48812f2d263806fe8cdf509bb9
Aakash Vishnuvarth <aakashvish07@gmail.com>
2025-10-02 19:52:49 -0500

    all_about_tsa

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hash = window.location.hash;
+    if(hash) {
+        const target = document.querySelector(hash);
+        if(target) {
+            setTimeout(()=>{
+                target.scrollIntoView({behavior: "smooth"});
+            },100);
+        }
+    }
+});
